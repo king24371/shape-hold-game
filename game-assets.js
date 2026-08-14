d5ed91ce22998db81b45e78a6a879598c78ec847
king24371 <109008158+king24371@users.noreply.github.com>
2026-08-14 11:48:34 +0800

    Add game-assets.js

---
+++ game-assets.js
@@ -0,0 +1,14 @@
+/**
+ * 蝝??亙嚗??單??獢?嚗耨?寥ㄐ?楝敺?胯? * ?單??征???剜嚗??MP3?GG?AV 蝑汗?券閮撘? */
+window.GAME_ASSETS = {
+  images: {
+    circle: "assets/circle.png",
+    doubleCircle: "assets/double-circle.png"
+  },
+  audio: {
+    growingLoop: "", // 靘?嚗?audio/orange-loop.mp3"
+    success: "",     // 靘?嚗?audio/success.mp3"
+    failure: ""      // 靘?嚗?audio/failure.mp3"
+  }
+};
+
